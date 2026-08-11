d9b47481bb19b19aa2549bc50027397a48444902
Pamu Harish <harishpamu96@gmail.com>
2026-08-11 13:01:50 +0530

    Use HTTPS for BusyBox repository cloning

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -72,7 +72,7 @@ mkdir -p var/log
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+git clone https://git.busybox.net/busybox
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
